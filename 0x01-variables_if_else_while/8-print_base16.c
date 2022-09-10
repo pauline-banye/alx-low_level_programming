df82e809e2d2eab3b22f906c48657babe420e0dc
pauline banye <paulinebanye@gmail.com>
2022-09-10 02:18:57 +0100

    edits

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,26 @@
-
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return:0
- */
+* main - Entry point
+*
+* Return: Always 0 (Succes)
+*/
+
 int main(void)
 {
-	int i, y;
-	for (i = 0; i < 10; i++)
+        int n;
+	
+	for (n = 0 ; n < 16 ; n++)
 	{
-		putchar (i + '0');
+		if (n < 10)
+		{
+			putchar('0' + n);
+		}
+		else
+		{
+			putchar(87 + n);
+		}
 	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-		putchar (y);
-	}
-
-putchar('\n');
-return (0);
-}
+	putchar('\n');
+	return (0);
+}				
