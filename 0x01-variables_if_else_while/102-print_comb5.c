c5a2397ed6f904b3b2be5905b38490b752836d37
pauline banye <paulinebanye@gmail.com>
2022-09-09 22:10:30 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,64 +1,73 @@
-#include <stdio.h> 
+/**                                                                                                                                     
 
- /** 
+ * main - Prints all possible combinations of two two-digit numbers,                                                                    
 
-  * main - printing all possible combinations of two digits 
+ * ranging from 0-99, separated by a comma followed by a space.                                                                         
 
-  * ranging from 0 - 99, separated by a comma folowed by a space 
+ * Return: Always 0.                                                                                                                    
 
-  * 
+ */                                                                                                                                     
 
-  * Return: 0. 
+                                                                                                                                        
 
-  */ 
+int main(void)                                                                                                                          
 
-  
+{                                                                                                                                       
 
-         int main(void) 
+                                                                                                                                        
 
- { 
+        int num1, num2;                                                                                                                 
 
-         int num1, num2; 
+                                                                                                                                        
 
-  
+        for (num1 = 0; num1 <= 98; num1++)                                                                                              
 
-         for (num1 = 0; num1 <= 98; num1++) 
+        {                                                                                                                               
 
-         { 
+                                                                                                                                        
 
-                 for (num2 = num1 + 1; num2 <= 99; num2++) 
+                for (num2 = num1 + 1; num2 <= 99; num2++)                                                                               
 
-                 { 
+                {                                                                                                                       
 
-  
+                        putchar((num1 / 10) + '0');                                                                                     
 
-                         putchar((num1 / 10) + '0'); 
+                        putchar((num1 % 10) + '0');                                                                                     
 
-                         putchar((num1 % 10) + '0');
-putchar('\n'); 
+                        putchar(' ');                                                                                                   
 
-                         putchar((num2 / 10) + '0'); 
+                        putchar((num2 / 10) + '0');                                                                                     
 
-                         putchar((num2 % 10) + '0'); 
+                        putchar((num2 % 10) + '0');                                                                                     
 
-  
+                                                                                                                                        
 
-                         if (num1 == 98 && num2 == 99) 
+                        if (num1 == 98 && num2 == 99)                                                                                   
 
-                                 continue; 
+                                                                                                                                        
 
-  
+                                continue;                                                                                               
 
-                         putchar (','); 
+                                                                                                                                        
 
-                         putchar (' '); 
+                        putchar(',');                                                                                                   
 
-                 } 
+                                                                                                                                        
 
-         } 
+                        putchar(' ');                                                                                                   
 
-  
+                                                                                                                                        
 
-         return (0); 
+                }                                                                                                                       
 
- }
+        }                                                                                                                               
+
+                                                                                                                                        
+
+        putchar('\n');                                                                                                                  
+
+                                                                                                                                        
+
+        return (0);                                                                                                                                                                                                                                                        
+
+}
