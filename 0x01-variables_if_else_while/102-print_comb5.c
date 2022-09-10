0c79157901c8a058e35ea44889b5f907401e2003
pauline banye <paulinebanye@gmail.com>
2022-09-09 23:07:32 -0700

    fixes 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,74 +1,30 @@
-/**                                                                                                                                     
+/**
+ * main - Prints all possible combinations of two two-digit numbers,
+ * ranging from 0-99,
+ * Return: Always 0.
+ */
 
- * main - Prints all possible combinations of two two-digit numbers,                                                                    
+int main(void)
 
- * ranging from 0-99, separated by a comma followed by a space.                                                                         
+{
+	 int num1, num2;
+	 for (num1 = 0; num1 <= 98; num1++)
+	 {
+		 for (num2 = num1 + 1; num2 <= 99; num2++)
+		 {
+			 putchar((num1 / 10) + '0');                          
+			 putchar((num1 % 10) + '0');                                                   putchar(' ');
+			 putchar((num2 / 10) + '0');
+			 putchar((num2 % 10) + '0');
 
- * Return: Always 0.                                                                                                                    
+			 if (num1 == 98 && num2 == 99)
+				 continue;
 
- */                                                                                                                                     
-
-                                                                                                                                        
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-                                                                                                                                        
-
-        int num1, num2;                                                                                                                 
-
-                                                                                                                                        
-
-        for (num1 = 0; num1 <= 98; num1++)                                                                                              
-
-        {                                                                                                                               
-
-                                                                                                                                        
-
-                for (num2 = num1 + 1; num2 <= 99; num2++)                                                                               
-
-                {                                                                                                                       
-
-                        putchar((num1 / 10) + '0');                                                                                     
-
-                        putchar((num1 % 10) + '0');                                                                                     
-
-                        putchar(' ');                                                                                                   
-
-                        putchar((num2 / 10) + '0');                                                                                     
-
-                        putchar((num2 % 10) + '0');                                                                                     
-
-                                                                                                                                        
-
-                        if (num1 == 98 && num2 == 99)                                                                                   
-
-                                                                                                                                        
-
-                                continue;                                                                                               
-
-                                                                                                                                        
-
-                        putchar(',');                                                                                                   
-
-                                                                                                                                        
-
-                        putchar(' ');                                                                                                   
-
-                                                                                                                                        
-
-                }                                                                                                                       
-
-        }                                                                                                                               
-
-                                                                                                                                        
-
-        putchar('\n');                                                                                                                  
-
-                                                                                                                                        
-
-        return (0);                                                                                                                                                                                                                                                        
-
+			 putchar(',');
+			 putchar(' ');
+		 }
+	}
+	 putchar('\n');
+	 return (0);
 }
 
