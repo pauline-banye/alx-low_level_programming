130ff641f1c3feb8789ea6d6f7ddbfd8f58cf40c
pauline banye <paulinebanye@gmail.com>
2022-09-15 02:33:51 +0100

    betty

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,24 +14,15 @@ int largest_number(int a, int b, int c)
 
 	if (a >= b && a >= c)
 	{
-
 		largest = a;
-
 	}
-
 	else if (b >= a && b >= c)
 	{
-
 		largest = b;
-
 	}
-
 	else
 	{
-	
 		largest = c;
-	
 	}
-
 	return (largest);
 }
