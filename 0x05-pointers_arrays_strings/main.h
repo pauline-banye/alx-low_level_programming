f87c423e71a33f25f9b6afbbe7fa7527c8bf160e
pauline banye <paulinebanye@gmail.com>
2022-09-20 00:30:24 +0100

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,11 @@
  * reset_to_98 - updates the variable
  * @n: pointer to the variable to update
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 void reset_to_98(int *n)
 {
 	*n = 98;
