48ff548e73a5f189896e9f0f61d5019f186b2720
pauline banye <paulinebanye@gmail.com>
2022-09-30 21:43:12 -0700

    fix betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
-
- * main - writes its name
-
+ * main - print file name
  * @argc: argument count
-
  * @argv: array of pointers to argument strings
-
  * Return: 0
-
  */
 
-
-
 int main(int argc, char *argv[])
-
-
-
 {
-
-	argc = argc;
-
-	printf("%s\n", *argv);
+	(void)argc;
+	printf("%s\n", argv[0]);
 
 	return (0);
 
