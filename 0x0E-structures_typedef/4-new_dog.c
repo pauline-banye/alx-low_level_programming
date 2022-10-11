340a5c5b1ae23f7c5e5c2f3f31636243115c8be1
pauline banye <paulinebanye@gmail.com>
2022-10-10 20:30:12 -0700

    error fixes

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,48 +6,40 @@
  * @name: pointer to name of dog
  * @age: age of dog
  * @owner: pointer to owner of dog
- * Return: pointer to a new dog of type my_dog
+ * Return: pointer to a new dog
  **/
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, lenX, lenY;
-	struct dog_t *puppy = NULL;
+	dog_t *puppy;
 
-	lenX = 0;
-	while (name[lenX != '\0'])
-		lenX++;
-	lenY = 0;
-	while owner[lenY != '\0'])
-		lenY++;
-	puppy = malloc(sizeof(struct dog));
-
+	puppy = (dog_t *)malloc(sizeof(dog_t));
 	if (puppy == NULL)
-	{
-		free(puppy);
 		return (NULL);
-	}
+	lenX = lenY = 0;
+	while (name[lenX++])
+		;
+	while (owner[lenY++])
+		;
 
-	puppy->name = malloc(lenX + 1);
+	puppy->name = malloc(lenX * sizeof(puppy->name));
 	if (puppy->name == NULL)
 	{
-		free(puppy->name);
-		free(puppy);
-		return (NULL);
-	}
-
-	puppy->owner = malloc(lenY + 1);
-	if (puppy->owner == NULL)
-	{
-		free(puppy->name);
-		free(puppy->owner);
 		free(puppy);
 		return (NULL);
 	}
 	for (i = 0; i <= lenX; i++)
 		puppy->name[i] = name[i];
+	puppy->age = age;
+	puppy->owner = malloc(lenY * sizeof(puppy->owner));
+	if (puppy->owner == NULL)
+	{
+		free(puppy->name);
+		free(puppy);
+		return (NULL);
+	}
 	for (i = 0; i <= lenY; i++)
 		puppy->owner[i] = owner[i];
-	puppy->age = age;
 	return (puppy);
 }
