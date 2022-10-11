2fe6d439f983bd1a1dbc8e9bb9638cbb678f3d03
pauline banye <paulinebanye@gmail.com>
2022-10-10 18:06:30 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,9 +16,12 @@ struct dog
 };
 
 typedef struct dog my_dog;
+
 void init_dog(struct dog *puppy, char *name, float age, char *owner);
 void print_dog(struct dog *puppy);
 my_dog *new_dog(char *name, float age, char *owner);
 void free_dog(my_dog *puppy);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
