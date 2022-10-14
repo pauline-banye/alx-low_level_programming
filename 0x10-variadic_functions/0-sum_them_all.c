3dc5b40b549e43f944ba2d3443f108df62ee87b1
pauline banye <paulinebanye@gmail.com>
2022-10-13 22:18:21 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,31 @@
-#include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 /**
- * sum_them_all - ...
- * @n: ...
- * @...:
- * Return: If n == 0 - 0.
- * Otherwise - the sum of all parameters.
- */
+* sum_them_all - The fxn sums of all its parameters
+* @n: n argument
+* @...: Accepts a Variable No. of arguments
+* hence a variadic Function
+* Return: sum or if n == 0, return 0
+*/
 
 int sum_them_all(const unsigned int n, ...)
-{
-	va_list nums;
-	unsigned int index, sum = 0;
 
-	va_start(nums, n);
-	for (index = 0; index < n; index++)
-		sum += va_arg(nums, int);
-	va_end(nums);
-		return (sum);
+{
+	int sum = 0;
+	unsigned int i;
+	va_list argument;
+
+	if (n)
+	{
+		va_start(argument, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(argument, int);
+		}
+
+		va_end(argument);
+	}
+	return (sum);
 }
